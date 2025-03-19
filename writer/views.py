1a1692a02d27a7b71cd77ccdcb6289e79a509e56
pansouk <pansouk@gmail.com>
2025-03-19 07:44:38 +0200

    Add writer dashboard template and navbar with logout functionality

--- writer/views.py
+++ writer/views.py
@@ -3,5 +3,5 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url="my-login")
-def writer_dashaboard(request):
-    return render(request, 'writer/writer_dashboard.html')
+def writer_dashboard(request):
+    return render(request, 'writer/writer-dashboard.html')
