41d06a53d1a64f1ad16befb13c70ee13ffd7ead9
pansouk <pansouk@gmail.com>
2025-03-19 06:39:58 +0200

    Add logout link and require login for client dashboard

--- client/views.py
+++ client/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='my-login')
 def client_dashboard(request):
     return render(request, 'client/client_dashboard.html')
